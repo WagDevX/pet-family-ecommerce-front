6f9468a081a9129d741265fb61c5979b678888e8
WagDevX <waguinbk@gmail.com>
2023-06-27 17:56:48 -0300

    SECRET added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
